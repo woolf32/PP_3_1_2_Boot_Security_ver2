58d61ab00873a550c269f7d19b622126f211ee25
йй <woolf_32@mail.ru>
2024-07-04 23:53:30 +0300

    add  hash,equals

--- src/main/java/ru/kata/spring/boot_security/demo/entity/Role.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entity/Role.java
@@ -8,6 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Objects;
 
 
 @Entity
@@ -61,5 +62,17 @@ public class Role implements GrantedAuthority {
                 '}';
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return id == role.id && Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
 
