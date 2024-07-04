4644cc241adb12e44abcda8fefd25dd8d288bf04
йй <woolf_32@mail.ru>
2024-07-05 00:24:25 +0300

    @ManyToMany(fetch = FetchType.LAZY)

--- src/main/java/ru/kata/spring/boot_security/demo/entity/Employee.java
+++ src/main/java/ru/kata/spring/boot_security/demo/entity/Employee.java
@@ -47,7 +47,7 @@ public class Employee implements UserDetails {
     @Column(name = "salary")
     private int salary;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
