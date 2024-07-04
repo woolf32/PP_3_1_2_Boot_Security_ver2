deedcde9cd7f23e9d1042c0916c3a77275fe10e6
йй <woolf_32@mail.ru>
2024-07-05 00:24:41 +0300

    @Query("select u from Employee u left join fetch u.roles where u.name=:username")

--- src/main/java/ru/kata/spring/boot_security/demo/repository/EmployeeRepository.java
+++ src/main/java/ru/kata/spring/boot_security/demo/repository/EmployeeRepository.java
@@ -3,10 +3,12 @@ package ru.kata.spring.boot_security.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import ru.kata.spring.boot_security.demo.entity.Employee;
+
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
-    @Query("SELECT u FROM Employee u WHERE u.name = :username")
+    @Query("select u from Employee u left join fetch u.roles where u.name=:username")
     Employee getUserByUsername(@Param("username") String username);
 }
