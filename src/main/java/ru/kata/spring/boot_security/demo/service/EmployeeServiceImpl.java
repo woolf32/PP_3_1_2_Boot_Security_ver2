22512635f48976e0d998d5749cd374cedea8650c
йй <woolf_32@mail.ru>
2024-08-15 22:40:58 +0300

    change method create and update

--- src/main/java/ru/kata/spring/boot_security/demo/service/EmployeeServiceImpl.java
+++ src/main/java/ru/kata/spring/boot_security/demo/service/EmployeeServiceImpl.java
@@ -12,6 +12,7 @@ import ru.kata.spring.boot_security.demo.repository.RoleRepository;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 
 @Service
@@ -43,25 +44,28 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     @Transactional
     @Override
-    public void save(Employee employee, List <Integer> selectedRoles) {
-        Employee savedEmployee = employeeRepository.save(employee);
-        savedEmployee.setPassword(bCryptPasswordEncoder.encode(employee.getPassword()));
-        List<Role> roles = roleRepository.findAllById(selectedRoles);
-        savedEmployee.setRoles(new HashSet<>(roles));
-        employeeRepository.save(savedEmployee);
+    public void save(Employee employee) {
+        employee.setPassword(bCryptPasswordEncoder.encode(employee.getPassword()));
+        Set<Role> detachedRoles = employee.getRoles();
+        Set<Role> attachedRoles = new HashSet<>();
+        for (Role role : detachedRoles) {
+            attachedRoles.add(roleRepository.findById(role.getId()).orElse(null));
+        }
+        employee.setRoles(attachedRoles);
+        employeeRepository.save(employee);
     }
 
     @Transactional
     @Override
-    public void update(int id, Employee employee, List <Integer> selectedRoles) {
-        Employee savedEmployee = employeeRepository.getById(id);
-        savedEmployee.setName(employee.getName());
-        savedEmployee.setSurname(employee.getSurname());
-        savedEmployee.setDepartment(employee.getDepartment());
-        savedEmployee.setSalary(employee.getSalary());
-        savedEmployee.setPassword(bCryptPasswordEncoder.encode(employee.getPassword()));
-        List<Role> roles = roleRepository.findAllById(selectedRoles);
-        savedEmployee.setRoles(new HashSet<>(roles));
+    public void update(Employee employee ) {
+        Employee savedEmployee = employeeRepository.findById(employee.getId()).orElseThrow(() ->
+                new RuntimeException("Employee not found with id: " + employee.getId()));
+       savedEmployee.updateFrom(employee,bCryptPasswordEncoder);
+        Set<Role> updatedRoles  = new HashSet<>();
+        for (Role role : employee.getRoles()) {
+            updatedRoles.add(roleRepository.findById(role.getId()).orElse(null));
+        }
+        savedEmployee.setRoles(updatedRoles);
         employeeRepository.save(savedEmployee);
 
     }
